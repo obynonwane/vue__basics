87722e76f8429bfa28b9172070d14f7043f6edb1
Obinna Johnson <o.johnson@nuture.tech>
2019-11-30 23:42:20 -0800

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Landing from '@/components/Landing'
 
 Vue.use(Router)
 
 export default new Router({
     routes:[
         {
-            path:'',
-            name:'',
-            component:''
+            path:'/',
+            name:'Landing',
+            component:Landing
         }
     ]
 })
